c6d99db8c68307f611ac1f451e12561e7c517e51
Артур А. Чубуклиев <10257@zp.itstep.org>
2018-10-15 20:23:40 +0300

    first

---
+++ ViewModelCL/Notifyer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ViewModelCL
+{
+    public class Notifyer : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WpfGitTest/AppViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ViewModelCL;
+
+namespace MainApp
+{
+    public class AppViewModel : Notifyer
+    {
+        #region Constructors
+        public AppViewModel()
+        {
+            Initializer();
+        }
+        #endregion
+        #region Methods
+        private void Initializer()
+        {
+
+        }
+
+        #endregion
+
+        #region Events
+        #region EventHadlers
+
+        #endregion
+        #endregion
+        #region Commands
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Fields
+        #endregion
+
+    }
+}
